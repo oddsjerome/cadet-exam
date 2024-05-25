c0b957ac9f2f491c84e12d13ff224b4b224a3d99
oddsjerome <naronkrach@odds.team>
2024-05-25 16:59:06 +0700

    Add link to nav icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,9 @@
             <nav class="bg-[#303030] py-4">
                 <Container>
                     <div class="flex justify-between">
-                        <img width="48px" src="../public/starbucks_logo_72px.png" alt="starbucks_logo" />
+                        <NuxtLink to="/">
+                            <img width="48px" src="../public/starbucks_logo_72px.png" alt="starbucks_logo" />
+                        </NuxtLink>
                         <div class="flex items-center gap-6 text-white text-sm">
                             <p>Jerome Tana</p>
                             <div class="w-12 h-12 rounded-full bg-white" />
