2c9dd6af0466572e61586a2e5422e73a604d1b07
oddsjerome <naronkrach@odds.team>
2024-05-25 22:48:25 +0700

    Adjust card styling

--- components/CoffeeCard.vue
+++ components/CoffeeCard.vue
@@ -10,10 +10,12 @@ const { coffee } = defineProps<{ coffee: Coffee }>()
             <div class="bg-primary h-52 rounded-lg flex justify-center items-center">
                 <img :src="coffee.image_url" :alt="coffee.name">
             </div>
-            <div class="pt-2">
-                <h2>{{ coffee.name }}</h2>
-                <Pill class="mt-2" :list="coffee.grind_option" />
-                <h3 class="pt-10 text-gray-500">{{ coffee.price }}$</h3>
+            <div class="pt-2 h-36 flex flex-col justify-between">
+                <div>
+                    <h2>{{ coffee.name }}</h2>
+                    <Pill class="mt-2" :list="coffee.grind_option" />
+                </div>
+                <h3 class="text-gray-500">{{ coffee.price }}$</h3>
             </div>
         </div>
     </NuxtLink>
