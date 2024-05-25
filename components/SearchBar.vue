b3425f1f7b1702393505e6d82ced4c1a05448f0b
oddsjerome <naronkrach@odds.team>
2024-05-25 19:22:01 +0700

    Refactor

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -5,17 +5,12 @@ const emit = defineEmits<{
     (e: 'search', query: string): void;
 }>();
 
-
-const onSearch = () => {
-    emit('search', query.value);
-};
-
 </script>
 <template>
     <div class="flex gap-2">
         <input type="search" name="search-input" placeholder="Starbuck product" id="" v-model="query"
             class="py-2 px-4 border rounded-full outline-none w-full">
         <input type="button" name="search-button" value="Search"
-            class="bg-black text-white px-5 py-1 rounded-full cursor-pointer" @click="onSearch">
+            class="bg-black text-white px-5 py-1 rounded-full cursor-pointer" @click="$emit('search',query)">
     </div>
 </template>
