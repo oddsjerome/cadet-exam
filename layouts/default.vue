9ead64a741bd4baeb7c59b6951f5007f92163161
oddsjerome <naronkrach@odds.team>
2024-05-25 12:34:39 +0700

    Add default layout (nav and footer)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-    <slot />
+    <nav class="bg-[#303030] p-4">
+        <Container>
+            <div class="flex justify-between">
+                <div class="w-5 h-5 bg-white"></div>
+                <div class="w-5 h-5 bg-white"></div>
+            </div>
+        </Container>
+    </nav>
+    <div class="min-h-screen">
+        <slot />
+    </div>
+
+    <footer class="bg-[#303030] p-2 flex justify-between text-white text-xs">
+        <p>©2024 Coffee Concepts Retail Co.,Ltd. All rights</p>
+        <p>Starbucks Thailand</p>
+    </footer>
 </template>
