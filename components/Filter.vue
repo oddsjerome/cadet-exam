f0f6142863ef8e54930efc3e37603a646eecc7c3
oddsjerome <naronkrach@odds.team>
2024-05-25 19:10:07 +0700

    Refactor

---
+++ components/Filter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div>
+        <div class="flex justify-between items-center">
+            <p>Filters</p>
+            <input type="button" value="Clear" class="border px-4 py-1 rounded-full cursor-pointer">
+        </div>
+        <div>
+            <div>
+                <h2>Category</h2>
+                <hr>
+            </div>
+            <div>
+                <h2>Roast</h2>
+                <hr>
+            </div>
+            <div>
+                <h2>Caffeine
+                    <hr>
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
