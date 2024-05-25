8fdbfdeeb52c8b864414c00b46d8f6d9717ccc1a
oddsjerome <naronkrach@odds.team>
2024-05-25 22:53:11 +0700

    Create UserItem component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,10 +5,7 @@
                 <NuxtLink to="/">
                     <img width="48px" src="../public/starbucks_logo_72px.png" alt="starbucks_logo" />
                 </NuxtLink>
-                <div class="flex items-center gap-6 text-white text-sm">
-                    <p>Jerome Tana</p>
-                    <div class="w-12 h-12 rounded-full bg-white" />
-                </div>
+                <UserItem name="Jerome Tana" />
             </div>
         </Container>
     </nav>
---
+++ components/UserItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    name: string
+}>()
+</script>
+
+<template>
+    <div class="flex items-center gap-6 text-white text-sm">
+        <p>{{ name }}</p>
+        <img src="https://thispersondoesnotexist.com/" class="w-12 h-12 rounded-full bg-white" />
+    </div>
+</template>
