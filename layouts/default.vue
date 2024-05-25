6c50948be8d0ea5886edc45b890f26c6371926da
oddsjerome <naronkrach@odds.team>
2024-05-25 16:11:01 +0700

    Modified footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-    <nav class="bg-[#303030] p-4">
-        <Container>
-            <div class="flex justify-between">
-                <div class="w-5 h-5 bg-white"></div>
-                <div class="w-5 h-5 bg-white"></div>
-            </div>
-        </Container>
-    </nav>
-    <div class="min-h-screen">
-        <slot />
-    </div>
+    <div class="min-h-screen flex flex-col justify-between">
+        <div>
+            <nav class="bg-[#303030] p-4">
+                <Container>
+                    <div class="flex justify-between">
+                        <div class="w-5 h-5 bg-white"></div>
+                        <div class="w-5 h-5 bg-white"></div>
+                    </div>
+                </Container>
+            </nav>
 
-    <footer class="bg-[#303030] p-2 flex justify-between text-white text-xs">
-        <p>©2024 Coffee Concepts Retail Co.,Ltd. All rights</p>
-        <p>Starbucks Thailand</p>
-    </footer>
+            <slot />
+        </div>
+        <footer class="bg-[#303030] p-2 text-xs text-white">
+            <Container>
+                <div class="flex justify-between">
+                    <p>©2024 Coffee Concepts Retail Co.,Ltd. All rights</p>
+                    <p>Starbucks Thailand</p>
+                </div>
+            </Container>
+        </footer>
+    </div>
 </template>
