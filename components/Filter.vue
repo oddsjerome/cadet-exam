8c272a8f003d8a06cf99d1a815e0540726b68fa7
oddsjerome <naronkrach@odds.team>
2024-05-26 00:05:43 +0700

    Create filter option with mock data

--- components/Filter.vue
+++ components/Filter.vue
@@ -1,25 +1,47 @@
 <script setup lang="ts">
+const grindOptions = [
+    { value: "Whole Bean" },
+    { value: "Cafetiere" },
+    { value: "Filter" },
+    { value: "Espresso" },
+    { value: "Pour Over" }
+];
+
+const flavorProfiles = [
+    { value: "Dark Chocolate" },
+    { value: "Black Cherry" },
+    { value: "Citrus" },
+    { value: "Toasted Nuts" },
+    { value: "Caramel" },
+    { value: "Cocoa" },
+    { value: "Hazelnut" },
+    { value: "Molasses" },
+    { value: "Smooth" },
+    { value: "Blueberry" },
+    { value: "Almond" },
+    { value: "Toffee" },
+    { value: "Blackcurrant" },
+    { value: "Coconut" },
+    { value: "Spicy" },
+    { value: "Earthy" },
+    { value: "Cinnamon" },
+    { value: "Clove" },
+    { value: "Honey" },
+    { value: "Chocolate" },
+    { value: "Floral" },
+    { value: "Vanilla" }
+];
+
 </script>
 <template>
     <div>
-        <div class="flex justify-between items-center">
-            <p>Filters</p>
+        <div class="flex justify-between items-center mb-10 ">
+            <p class="text-lg font-semibold">Filters</p>
             <Button value="Clear" :isLeftIcon="true" variable="Outlined" icon="" :click="() => { }" />
         </div>
         <div>
-            <div>
-                <h2>Category</h2>
-                <hr>
-            </div>
-            <div>
-                <h2>Roast</h2>
-                <hr>
-            </div>
-            <div>
-                <h2>Caffeine
-                    <hr>
-                </h2>
-            </div>
+            <FilterOption name="Grind option" :options="grindOptions" />
+            <FilterOption name="Flavor profile" :options="flavorProfiles" />
         </div>
     </div>
 </template>
---
+++ components/FilterOption.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    name: string,
+    options: { value: string }[]
+}>();
+</script>
+
+<template>
+    <div class="mb-8">
+        <h2 class="mb-2">{{ name }}</h2>
+        <hr class="mb-4">
+        <ul class="space-y-2">
+            <li v-for="(option, index) in options" :key="index" class="flex items-center">
+                <input :value="option.value" :id="option.value" type="checkbox"
+                    class="form-checkbox h-5 w-5 text-primary rounded" />
+                <label :for="option.value" class="ml-2 text-gray-500">{{ option.value }}</label>
+            </li>
+        </ul>
+    </div>
+</template>
+  
+ 
