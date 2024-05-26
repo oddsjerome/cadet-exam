c056457ba59c1074c921de7a003c42cdff1949e8
oddsjerome <naronkrach@odds.team>
2024-05-26 08:22:17 +0700

    Set font bold on name card

--- components/CoffeeCard.vue
+++ components/CoffeeCard.vue
@@ -12,7 +12,7 @@ const { coffee } = defineProps<{ coffee: Coffee }>()
             </div>
             <div class="pt-2 h-36 flex flex-col justify-between">
                 <div>
-                    <h2>{{ coffee.name }}</h2>
+                    <h2 class="font-bold">{{ coffee.name }}</h2>
                     <Pill class="mt-2" :list="coffee.grind_option" />
                 </div>
                 <h3 class="text-gray-500">{{ coffee.price }}$</h3>
