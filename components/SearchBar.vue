31b4a70ffd15dcde02dc17840bf50277e2d7716b
oddsjerome <naronkrach@odds.team>
2024-05-27 15:29:41 +0700

    Add keyup.enter on search bar

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -19,7 +19,7 @@ watch(query, () => {
                 <IconsSearch class="fill-black h-full w-10 pl-4" />
             </label>
             <input type="search" name="search-input" id="search-input" placeholder="Starbucks product" v-model="query"
-                class="py-2 px-4 border-y border-r rounded-r-full outline-none w-full">
+                @keyup.enter="$emit('search', query)" class="py-2 px-4 border-y border-r rounded-r-full outline-none w-full">
         </div>
         <Button value="Search" :isLeftIcon="true" variable="secondary-filled" :isDisabled="!query"
             :click="() => { $emit('search', query) }" />
