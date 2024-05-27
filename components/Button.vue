75f571747c38ee538b3bcfa45e861a8c39e9d6e6
oddsjerome <naronkrach@odds.team>
2024-05-27 08:15:52 +0700

    Add button hover effect

--- components/Button.vue
+++ components/Button.vue
@@ -10,20 +10,20 @@ const props = defineProps<{
 }>();
 
 const buttonClasses = computed(() => {
-    let baseClasses = "border px-3 py-2 rounded-full flex items-center justify-center space-x-2";
+    let baseClasses = "border px-3 py-2 rounded-full flex items-center justify-center space-x-2 duration-200";
 
     switch (props.variable) {
         case "primary-filled":
             baseClasses += " bg-primary text-white fill-white";
             break;
         case "primary-outlined":
-            baseClasses += " border-primary text-primary fill-primary";
+            baseClasses += " border-primary text-primary fill-primary hover:bg-gray-100";
             break;
         case "secondary-filled":
             baseClasses += " bg-secondary text-white fill-white";
             break;
         case "secondary-outlined":
-            baseClasses += " border-secondary text-secondary fill-secondary";
+            baseClasses += " border-secondary text-secondary fill-secondary hover:bg-gray-100";
             break;
     }
 
